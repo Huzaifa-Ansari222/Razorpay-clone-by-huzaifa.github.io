5af118e57429f49cffd1042456b2b2cf5cff7acf
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2023-10-21 16:46:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mullish","sans-serif"],
+      },
+    colors: {
+    deepBlue:"#02042a",
+    lightBlue: "#2b84ea",
+    lightBlue300: "#4b94ed",
+    deepBlue: "#02042a", 
+    lightBlue500: "#0b72e7", 
+    greenLight: "#61cea6", 
+    grayText: "#818597", 
+    LightGray: "#e2e2e2", 
+    gray2: "#525a76",
+    grayBlue: "#344a6c", 
+    deepBlueHead:"#162f56",
+    },},
+  },
+  plugins: [],
+}
